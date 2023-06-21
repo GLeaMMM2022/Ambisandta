bfdd5967b18dde167984dc100f62684c8043f8b9
forstudyas2004@gmail.com <forstudyas2004@gmail.com>
2023-06-21 21:09:37 +0300

    Добавьте файлы проекта.

---
+++ Ambisandta2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace OssetianProverbsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ossetianListBox = new System.Windows.Forms.ListBox();
+            this.russianListBox = new System.Windows.Forms.ListBox();
+            this.proverbsListView = new System.Windows.Forms.ListView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.rulesButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ossetianListBox
+            // 
+            this.ossetianListBox.FormattingEnabled = true;
+            this.ossetianListBox.ItemHeight = 25;
+            this.ossetianListBox.Location = new System.Drawing.Point(177, 295);
+            this.ossetianListBox.Name = "ossetianListBox";
+            this.ossetianListBox.Size = new System.Drawing.Size(315, 254);
+            this.ossetianListBox.TabIndex = 0;
+            // 
+            // russianListBox
+            // 
+            this.russianListBox.FormattingEnabled = true;
+            this.russianListBox.ItemHeight = 25;
+            this.russianListBox.Location = new System.Drawing.Point(643, 295);
+            this.russianListBox.Name = "russianListBox";
+            this.russianListBox.Size = new System.Drawing.Size(315, 254);
+            this.russianListBox.TabIndex = 1;
+            // 
+            // proverbsListView
+            // 
+            this.proverbsListView.FullRowSelect = true;
+            this.proverbsListView.HideSelection = false;
+            this.proverbsListView.Location = new System.Drawing.Point(1030, 246);
+            this.proverbsListView.Name = "proverbsListView";
+            this.proverbsListView.Size = new System.Drawing.Size(995, 369);
+            this.proverbsListView.TabIndex = 2;
+            this.proverbsListView.UseCompatibleStateImageBehavior = false;
+            this.proverbsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(317, 674);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(175, 66);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(643, 674);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(146, 66);
+            this.checkButton.TabIndex = 4;
+            this.checkButton.Text = "Проверить";
+            this.checkButton.UseVisualStyleBackColor = true;
+            // 
+            // rulesButton
+            // 
+            this.rulesButton.Location = new System.Drawing.Point(856, 56);
+            this.rulesButton.Name = "rulesButton";
+            this.rulesButton.Size = new System.Drawing.Size(146, 66);
+            this.rulesButton.TabIndex = 5;
+            this.rulesButton.Text = "button1";
+            this.rulesButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2083, 937);
+            this.Controls.Add(this.rulesButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.proverbsListView);
+            this.Controls.Add(this.russianListBox);
+            this.Controls.Add(this.ossetianListBox);
+            this.Name = "MainForm";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ossetianListBox;
+        private System.Windows.Forms.ListBox russianListBox;
+        private System.Windows.Forms.ListView proverbsListView;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button rulesButton;
+    }
+}
+
---
+++ Ambisandta2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace OssetianProverbsApp
+{
+    public partial class MainForm : Form
+    {
+        private List<string> ossetianProverbs;
+        private List<string> russianTranslations;
+        private List<string> selectedProverbs;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            proverbsListView.View = View.Details;
+            proverbsListView.Columns.Add("На осетинском");
+            proverbsListView.Columns.Add("На русский");
+            proverbsListView.Columns[0].Width = 200;
+            proverbsListView.Columns[1].Width = 200;
+            InitializeProverbs();
+            InitializeListBoxes();
+            selectedProverbs = new List<string>();
+        }
+
+        private void InitializeProverbs()
+        {
+            // Здесь вы можете заполнить списки пословицами на осетинском и их переводами на русский
+            ossetianProverbs = new List<string>
+            {
+                "Пословица на осетинском 1",
+                "Пословица на осетинском 2",
+                "Пословица на осетинском 3"
+            };
+
+            russianTranslations = new List<string>
+            {
+                "Перевод на русский 1",
+                "Перевод на русский 2",
+                "Перевод на русский 3"
+            };
+        }
+
+        private void InitializeListBoxes()
+        {
+            // Заполняем ListBox пословицами на осетинском
+            foreach (var proverb in ossetianProverbs)
+            {
+                ossetianListBox.Items.Add(proverb);
+            }
+
+            // Заполняем ListBox переводами пословиц на русский
+            foreach (var translation in russianTranslations)
+            {
+                russianListBox.Items.Add(translation);
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            // Получаем выбранные элементы из ListBox-ов
+            if (ossetianListBox.SelectedItem != null && russianListBox.SelectedItem != null)
+            {
+                string selectedProverb = ossetianListBox.SelectedItem.ToString();
+                string selectedTranslation = russianListBox.SelectedItem.ToString();
+
+                // Добавляем выбранные пословицу и перевод в ListView
+                ListViewItem listViewItem = new ListViewItem(new[] { selectedProverb, selectedTranslation });
+                proverbsListView.Items.Add(listViewItem);
+                selectedProverbs.Add(selectedProverb);
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите пословицу и ее перевод.");
+            }
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            // Проверяем правильность выбранных переводов
+            foreach (ListViewItem item in proverbsListView.Items)
+            {
+
+                string proverb = item.SubItems[0].Text;
+                string translation = item.SubItems[1].Text;
+                int index = ossetianProverbs.IndexOf(proverb);
+
+                if (index != -1 && russianTranslations[index] == translation)
+                {
+                    // Правильный перевод
+                    item.BackColor = System.Drawing.Color.LightGreen;
+                }
+                else
+                {
+                    // Неправильный перевод
+                    item.BackColor = System.Drawing.Color.LightPink;
+                }
+            }
+        }
+
+        
+    }
+}
